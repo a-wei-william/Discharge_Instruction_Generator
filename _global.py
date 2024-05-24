3753b4e62da452c8192070a89fc0b181c03d8a48
William <a.wei.william.0513@gmail.com>
2024-05-24 12:22:11 -0700

    update the depreciated langchain import

--- _global.py
+++ _global.py
@@ -9,7 +9,7 @@ path_to_resources = "./resources/"
 
 
 # set up embedding transformer
-from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 hf_embed = HuggingFaceBgeEmbeddings(
     model_name = "BAAI/bge-small-en", 
     model_kwargs = {"device": "cpu"}, 
