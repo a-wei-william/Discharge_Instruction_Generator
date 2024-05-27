2445cef0a1567d56ea5bcb396cad2d14eb90dc2d
William <a.wei.william.0513@gmail.com>
2024-05-27 15:35:18 -0700

    removed queries_ddx dict. Changed prompts to chatprompt style

--- templates.py
+++ templates.py
@@ -4,50 +4,7 @@
 
 
 # LLM
-discharge_instructions = """
-    Instruction:
-        You are a doctor in the emergency department preparing a discharge instruction handout for a patient. 
-        You will be provided with information regarding a diagnosis in the Context below.
-        Use the provided Context to fill in the Template below. 
-        Include the source of the information at the end of the sentence. Include a reference list at the end of the document.
-        Write in clear, concise, jargon-free style. The instructions should be directed to the parent and address the patient as "your child". 
-        If you are unsure or if no relevant information was retrieved, just say you don't know. Don't make up information or hallucinate.
-    ---
-    Context:
-        Information on the definition: {context_definition}
-
-
-        Information on the presentation: {context_presentation}
-
-
-        Information on the course: {context_course}
-
-
-        Information on supportive management: {context_management_supportive}
-
-
-        Information on treatment: {context_management_pharmacologic}
-
-
-        Information on follow-up plan : {context_follow_up}
-
-
-        Information on when to seek urgent medical attention : {context_redflags}
-
-
-    ---
-    Template:
-        Diagnosis: <Insert diagnosis>
-
-        <Insert explanation of the main diagnosis using information in Context>
-
-        <Insert when to seek urgent medical attention using Information on when to seek urgent medical attention>
-
-        <Insert reference list>
-"""
-
-
-discharge_instructions_2 = """
+handout_generation = """
     Instruction:
         You are a doctor in the emergency department preparing a discharge instruction handout for a patient. 
         You will be provided with information regarding a diagnosis in the CONTEXT below, which you will use to generate the handout by following the exact format of the TEMPLATE below.
@@ -97,51 +54,26 @@ discharge_instructions_2 = """
 
 
 extract_diagnosis = """
-    Extract the diagnosis from the given text below. Output should only include the diagnosis.
+    You are a doctor extracting diagnosis from a provided assessment. Only output the diagnosis, DO NOT output any other texts.
     ---
-    Text: {assessment}
+    {assessment}
 """
 
 
 compress_context = """
-Task: Extract information that can be used to answer the given query from each of the documents in the given context.
-Details:
-- Extract the information verbatim from the context. 
-- Use the output template below to format the extracted information from each document; [] are fields that you need to fill in.
+    You are a summary robot that is tasked to extract information from each document that can be used to answer the given query.
+    Extract the information verbatim from the context. 
+    Use the output template below to format the extracted information from each document. [] are fields that you need to fill in.
 
-### Query ###
+### QUERY ###
 {query}
 
-### Context ###
+### CONTEXT ###
 {context}
 
-### Output Template ###
+### OUTPUT TEMPLATE ###
 Doc: [Document number]
-Context: [content extracted from the documen]
+Context: [content extracted from the document]
 Source: [source of the document]
 \n
 """
-
-
-# DB queries
-"""
-the template should include information on:
-- definition of diagnosis
-- how diagnosis presents
-- the course of diagnosis
-- management for diagnosis
-    - supportive
-    - pharmacologic
-    - follow-up
-- red flags to look out for
-"""
-
-queries_ddx = {
-    "definition": "definition of {diagnosis}",
-    "presentation": "manifestations of {diagnosis}",
-    "course": "natural history of {diagnosis}",
-    "management_supportive": "conservative treatment for {diagnosis}",
-    "management_pharmacologic": "treatment for {diagnosis}",
-    "follow_up": "follow-up plan for {diagnosis}",
-    "redflags": "signs and symptoms that indicate the need for urgent medical attention for patients with {diagnosis}",
-}
