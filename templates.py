0fce7ca3654d387950c57a6f25d5e5c8acdf8dbe
William <a.wei.william.0513@gmail.com>
2024-05-28 12:55:59 -0700

    differentiate between system and human prompts

--- templates.py
+++ templates.py
@@ -4,7 +4,7 @@
 
 
 # LLM
-handout_generation = """
+handout_generation_system = """
     Instruction:
         You are a doctor in the emergency department preparing a discharge instruction handout for a patient. 
         You will be provided with information regarding a diagnosis in the CONTEXT below, which you will use to generate the handout by following the exact format of the TEMPLATE below.
@@ -53,27 +53,39 @@ handout_generation = """
 """
 
 
-extract_diagnosis = """
-    You are a doctor extracting diagnosis from a provided assessment. Only output the diagnosis, DO NOT output any other texts.
-    ---
-    {assessment}
+handout_generation_human = """
+    CONTEXT
+    Information on the Definition of the disease: {context_definition}. \n
+    Information on the Presentation of the disease: {context_presentation}. \n
+    Information on the Course of the disease: {context_course}. \n
+    Information on Management of the disease: {context_management}, {context_follow_up}. \n
+    Doctor's input on Management of the disease: {context_md_plan}. \n
+    Information on when to seek urgent medical attention for the disease: {context_redflags}. \n
+
 """
 
 
-compress_context = """
+extract_diagnosis_system = """
+    You are a doctor extracting diagnosis from a provided assessment. Only output the diagnosis, DO NOT output any other texts.
+"""
+
+
+compress_context_system = """
     You are a summary robot that is tasked to extract information from each document that can be used to answer the given query.
     Extract the information verbatim from the context. 
-    Use the output template below to format the extracted information from each document. [] are fields that you need to fill in.
-
-### QUERY ###
-{query}
-
-### CONTEXT ###
-{context}
+    Use the output template below to format the extracted information from each document. [] are fields that you need to fill in. Do not include the header in ###
 
 ### OUTPUT TEMPLATE ###
 Doc: [Document number]
 Context: [content extracted from the document]
 Source: [source of the document]
 \n
+"""
+
+compress_context_human = """
+    ### QUERY ###
+    {query}
+
+    ### CONTEXT ###
+    {context}
 """
