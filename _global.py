0f8d127ce46318747a4d506f660f039cd907498e
William <a.wei.william.0513@gmail.com>
2024-06-28 15:17:33 -0700

    changed embedding to BAAI/llm-embedder

--- _global.py
+++ _global.py
@@ -11,7 +11,7 @@ path_to_resources = "./resources/"
 # set up embedding transformer
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 hf_embed = HuggingFaceBgeEmbeddings(
-    model_name = "BAAI/bge-small-en", 
+    model_name = "BAAI/llm-embedder", 
     model_kwargs = {"device": "cpu"}, 
     encode_kwargs = {"normalize_embeddings": True}
 )
